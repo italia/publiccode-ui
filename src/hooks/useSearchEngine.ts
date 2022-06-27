007efaf7b6f17f4510596d0e916cc9c76962b858
Alessandro Sebastiani <sebbalex@gmail.com>
2022-06-27 20:32:00 +0200

    fix: typing for redux actions which involve searching

--- src/hooks/useSearchEngine.ts
+++ src/hooks/useSearchEngine.ts
@@ -1,11 +1,11 @@
-import { useContext, useEffect, useReducer, useRef } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { useFlexSearch } from 'react-use-flexsearch';
-import { searchContextDispatch, searchContextState, incrementPage } from '../contexts/searchContext';
-const ADD_ITEMS = 'ADD_ITEMS';
-const SET_ITEMS = 'SET_ITEMS';
-const SET_IS_LOADING = 'SET_IS_LOADING';
-const SET_ERROR = 'SET_ERROR';
+import { useContext, useEffect, useReducer, useRef } from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { useFlexSearch } from "react-use-flexsearch";
+import {
+  searchContextDispatch,
+  searchContextState,
+  incrementPage,
+} from "../contexts/searchContext";
 
 enum ActionType {
   ADD_ITEMS,
@@ -17,7 +17,7 @@ interface ActionAddItems {
   type: ActionType.ADD_ITEMS;
   value: {
     items: string[];
-  }
+  };
 }
 
 interface ActionSetItems {
@@ -25,7 +25,7 @@ interface ActionSetItems {
   value: {
     items: string[];
     total: number;
-  }
+  };
 }
 
 interface ActionSetIsLoading {
@@ -36,10 +36,14 @@ interface ActionSetError {
   type: ActionType.SET_ERROR;
   value: {
     errorMessage: string;
-  }
+  };
 }
 
-type Actions = ActionAddItems | ActionSetItems | ActionSetIsLoading | ActionSetError;
+type Actions =
+  | ActionAddItems
+  | ActionSetItems
+  | ActionSetIsLoading
+  | ActionSetError;
 
 interface State {
   isLoading: boolean;
@@ -89,19 +93,32 @@ const reducer = (state: State, action: Actions) => {
   return state;
 };
 
-const areMoreItemsAvailable = (from: number, size: number, total: number) => from + size < total;
+const areMoreItemsAvailable = (from: number, size: number, total: number) =>
+  from + size < total;
 
 export const useSearchEngine = ({ pageSize } = { pageSize: 12 }) => {
-  const [{ items, total, isLoading, errorMessage }, dispatch] = useReducer(reducer, initial);
+  const [{ items, total, isLoading, errorMessage }, dispatch] = useReducer(
+    reducer,
+    initial
+  );
   const dispatchGlobal = useContext(searchContextDispatch);
-  const { filterCategories, filterDevelopmentStatuses, filterIntendedAudiences, page, type, searchValue, sortBy } =
-    useContext(searchContextState);
+  const {
+    filterCategories,
+    filterDevelopmentStatuses,
+    filterIntendedAudiences,
+    page,
+    type,
+    searchValue,
+    sortBy,
+  } = useContext(searchContextState);
   // This feature is mainly used by the infiniteScroll to reload the previous items just after an user click on the browser back button
-  const reloadItemsUntilPage = useRef(page > 0 ? page : false);
+  const reloadItemsUntilPage = useRef(page > 0 ? page : null);
   // If we aren't in the "resume mode" the from is incremental
   const from = reloadItemsUntilPage.current ? 0 : page * pageSize;
   // If we aren't in the "resume mode" use the default page size
-  const size = reloadItemsUntilPage.current ? (reloadItemsUntilPage.current + 1) * pageSize : pageSize;
+  const size = reloadItemsUntilPage.current
+    ? (reloadItemsUntilPage.current + 1) * pageSize
+    : pageSize;
 
   const {
     localSearchPages: { index, store },
@@ -120,16 +137,19 @@ export const useSearchEngine = ({ pageSize } = { pageSize: 12 }) => {
     }
   };
 
-  const results = useFlexSearch(searchValue, index, store);
+  const results: string[] = useFlexSearch(searchValue, index, store);
 
   useEffect(() => {
-    dispatch({
-      type: from === 0 ? SET_ITEMS : ADD_ITEMS,
-      value: {
-        items: results,
-        total: results.length,
-      },
-    });
+    const set: ActionSetItems = {
+      type: ActionType.SET_ITEMS,
+      value: { items: results, total: results.length },
+    };
+    const add: ActionAddItems = {
+      type: ActionType.ADD_ITEMS,
+      value: { items: results },
+    };
+
+    dispatch(from === 0 ? set : add);
   }, [from, results]);
 
   return [errorMessage, items, total, fetchMore];
