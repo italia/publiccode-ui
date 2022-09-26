67fd575c2906aa05af7e5177865a500856042cf1
Fabio Bonelli <fb@fabiobonelli.it>
2022-09-26 11:04:58 +0200

    chore(ui): use layout for search page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,18 @@ import { useTranslation } from 'react-i18next';
 import '../i18n';
 
 import { CatalogueContainer } from "../components/Catalogue/CatalogueContainer";
+import { Page } from "../components/Page/Page";
 
 const IndexPage = () => {
   const { t } = useTranslation();
 
   return (
-    <>
+    <Page>
       <div className="container">
         <CatalogueContainer />
         {/* <CatalogueItem fallback="fallback" /> */}
       </div>
-    </>
+    </Page>
   );
 };
 
